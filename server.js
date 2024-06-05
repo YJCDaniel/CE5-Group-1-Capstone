5e14239c889caaee1b0b3136f40cbfd9d806e976
James_Tan <jmesthy@yahoo.com.sg>
2024-06-05 23:17:22 +0800

    edited server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 // Serve static files from the "public" directory
-app.use(express.static);
+app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html');
+    res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-});
+}); 
