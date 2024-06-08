fe8a985411a5e332ffc739314cbf5fb9560ad4c3
James_Tan <jmesthy@yahoo.com.sg>
2024-06-08 23:34:16 +0800

    allow cors in web server

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
+
 app.use(express.static(path.join(__dirname, '/')));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
