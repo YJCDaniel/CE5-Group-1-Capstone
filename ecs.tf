0e5e1413ad96c248b8b68d53eb26559468bb3c2a
James_Tan <jmesthy@yahoo.com.sg>
2024-06-12 23:40:28 +0800

    edited HTML

--- ecs.tf
+++ ecs.tf
@@ -1,51 +1,36 @@
-# Creates a task definition using the files/task-definition.json
-# HINT: if you would like to use your own docker image, update the container_definitions image_url
-# accordingly
-
 resource "aws_ecs_task_definition" "own_task_definition" {
-  family                    = var.ecs_task_defn_family # Update accordingly
+  family                    = var.ecs_task_defn_family
   requires_compatibilities  = ["FARGATE"]
   network_mode              = "awsvpc"
   execution_role_arn        = var.ex_role_arn
   cpu                       = 2048
   memory                    = 4096
 
-  # For custom ECR image
-  # container_definitions = templatefile("./files/task-definition.json", {
-  #   image_url        = "255945442255.dkr.ecr.us-east-1.amazonaws.com/luqman-test-ecr-repo:latest"
-  #   container_name   = "CE5-Group1-ecs"
-  #   port_name        = "CE5-Group1-ecs-8080-tcp"
-  #   log_group_region = "us-east-1"
-  #   log_group_name   = "/ecs/CE5grp1taskdef"
-  #   log_group_prefix = "ecs"
-  # })
-
-  # For nginx image
+  
   container_definitions = templatefile("./files/ecr-task-definition.json", {
     image_url        = "${var.ecr_url}/${var.ecr_image_name}:latest"
     port_name        = var.ecs_port_name
     container_name   = var.ecs_container_name
     log_group_region = "us-east-1"
-    log_group_name   = "/ecs/${var.ecs_task_defn_family}" # Change accordingly
+    log_group_name   = "/ecs/${var.ecs_task_defn_family}"
     log_group_prefix = "ecs"
   })
 }
 
 resource "aws_ecs_cluster" "own_cluster" {
-  name = var.ecs_cluster_name # Update in variables.tf
+  name = var.ecs_cluster_name
 }
 
-# Creates an ecs service
 
 resource "aws_ecs_service" "own_service" {
-  name             = var.ecs_service_name # Update in variables.tf
+  name             = var.ecs_service_name 
   cluster          = aws_ecs_cluster.own_cluster.arn
   task_definition  = aws_ecs_task_definition.own_task_definition.arn
   desired_count    = 1
   launch_type      = "FARGATE"
   platform_version = "LATEST"
 
-  # Force a new deployment of task definition
+  
   force_new_deployment = true
 
   deployment_circuit_breaker {
