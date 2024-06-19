14eaca20ab9f9824c86373d6f68e649a523dc22a
Daniel Yang <yjcdaniel@hotmail.com>
2024-06-20 03:29:09 +0800

    edit ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,3 @@
 resource "aws_ecr_repository" "new" {
-  count = length(data.aws_ecr_repository.existing.*.name) > 0 ? 0 : 1
-  name  = var.ecr_repository_name
-}
-data "aws_ecr_repository" "existing" {
-  name = var.ecr_repository_name
+  name = var.repository_name
 }
